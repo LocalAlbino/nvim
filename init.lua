e05cebf76e770429e48d54d4e941ec5d2c1d5c98
LocalAlbino <rudyhartwig25@outlook.com>
2024-11-10 18:13:29 -0500

    update mason and other changes

---
+++ init.lua
@@ -0,0 +1,5 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+vim.cmd([[set tabstop=4
+    set shiftwidth=4
+  ]])
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  "navarasu/onedark.nvim",
+  priority = 1000,
+  opts = {
+    style = "deep",
+    transparent = true,
+  },
+  config = function(_, opts)
+    local onedark = require("onedark")
+    onedark.setup(opts)
+    onedark.load()
+  end,
+}
