b1fd808c815569d9a1c8e567720f1139348085ec
LocalAlbino <rudyhartwig25@outlook.com>
2024-12-19 04:06:38 -0500

    added moonfly colorscheme

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.o.guifont = "Hack Nerd Font Mono:h22"
+vim.o.guifont = "Hack Nerd Font Mono:h22" -- Set font and size for neovide
+vim.cmd([[colorscheme moonfly]]) -- Set colorscheme (moonfly)
+--vim.cmd([[colorscheme onedark]]) -- Set colorscheme (onedark)
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,20 @@
 return {
-    "navarasu/onedark.nvim",
+    -- Onedark
+    --"navarasu/onedark.nvim",
+    --priority = 1000,
+    --opts = {
+    --style = "deep",
+    --transparent = false,
+    --},
+    --config = function(_, opts)
+    --local onedark = require("onedark")
+    --onedark.setup(opts)
+    --onedark.load()
+    --end,
+
+    -- Moonfly
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
     priority = 1000,
-    opts = {
-        style = "deep",
-        transparent = false,
-    },
-    config = function(_, opts)
-        local onedark = require("onedark")
-        onedark.setup(opts)
-        onedark.load()
-    end,
+    lazy = false,
 }
