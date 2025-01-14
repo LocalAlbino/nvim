1bc5d34797ee2fa2b986718ce7abcba3aea969a5
LocalAlbino <rudyhartwig25@outlook.com>
2025-01-13 22:05:01 -0500

    update: changed colorscheme once again :)

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.o.guifont = "Hack Nerd Font Mono:h22" -- Set font and size for neovide
-vim.cmd([[colorscheme moonfly]]) -- Set colorscheme (moonfly)
---vim.cmd([[colorscheme onedark]]) -- Set colorscheme (onedark)
+vim.cmd([[colorscheme onedark]]) -- Set colorscheme (onedark)
