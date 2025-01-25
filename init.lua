6a1eb62c0446443e04f173497480a8f0638a35ee
LocalAlbino <rudyhartwig25@outlook.com>
2025-01-25 17:10:50 -0500

    add keycommands for font sizing

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.o.guifont = "Hack Nerd Font Mono:h22" -- Set font and size for neovide
-vim.cmd([[colorscheme onedark]]) -- Set colorscheme (onedark)
+
+vim.cmd.colorscheme("onedark") -- Set colorscheme (onedark)
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,22 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local fontSize = 22 -- default font size
+vim.o.guifont = "Hack Nerd Font Mono:h" .. fontSize -- Set default font and size
+
+-- Keymaps for increasing or decreasing font size
+vim.keymap.set("n", "<C-p>", function()
+    fontSize = fontSize + 1
+    print("Font size is now " .. fontSize)
+    vim.o.guifont = "Hack Nerd Font Mono:h" .. fontSize
+end)
+vim.keymap.set("n", "<C-m>", function()
+    fontSize = fontSize - 1
+    print("Font size is now " .. fontSize)
+    vim.o.guifont = "Hack Nerd Font Mono:h" .. fontSize
+end)
+
+-- Sync OS and Neovim clipboard
+vim.schedule(function()
+    vim.opt.clipboard = "unnamedplus"
+end)
