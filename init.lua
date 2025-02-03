be0488e7e018aab7425568cc466ea6806ac89e0b
LocalAlbino <rudyhartwig25@outlook.com>
2025-02-03 01:51:41 -0500

    added some neovide specific config settings

--- init.lua
+++ init.lua
@@ -2,3 +2,16 @@
 require("config.lazy")
 
 vim.cmd.colorscheme("onedark") -- Set colorscheme (onedark)
+
+-- Sync OS and Neovim clipboard
+vim.schedule(function()
+    vim.opt.clipboard = "unnamedplus"
+end)
+
+-- Set some neovide settings
+if vim.g.neovide then
+    vim.g.neovide_refresh_rate = 165
+    vim.g.neovide_cursor_animation_length = 0
+    vim.g.neovide_position_animation_length = 0
+    vim.g.neovide_scroll_animation_length = 0
+end
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,22 +1,20 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-local fontSize = 22 -- default font size
-vim.o.guifont = "Hack Nerd Font Mono:h" .. fontSize -- Set default font and size
 
--- Keymaps for increasing or decreasing font size
-vim.keymap.set("n", "<C-p>", function()
-    fontSize = fontSize + 1
-    print("Font size is now " .. fontSize)
-    vim.o.guifont = "Hack Nerd Font Mono:h" .. fontSize
-end)
-vim.keymap.set("n", "<C-m>", function()
-    fontSize = fontSize - 1
-    print("Font size is now " .. fontSize)
-    vim.o.guifont = "Hack Nerd Font Mono:h" .. fontSize
-end)
+-- Only need these keymaps if using neovide
+if vim.g.neovide then
+    local fontSize = 22 -- default font size
+    vim.o.guifont = "Hack Nerd Font Mono:h" .. fontSize -- Set default font and size
 
--- Sync OS and Neovim clipboard
-vim.schedule(function()
-    vim.opt.clipboard = "unnamedplus"
-end)
+    -- Keymaps for increasing or decreasing gui font size
+    vim.keymap.set("n", "<C-p>", function()
+        fontSize = fontSize + 1
+        print("Font size is now " .. fontSize)
+        vim.o.guifont = "Hack Nerd Font Mono:h" .. fontSize
+    end)
+    vim.keymap.set("n", "<C-m>", function()
+        fontSize = fontSize - 1
+        print("Font size is now " .. fontSize)
+        vim.o.guifont = "Hack Nerd Font Mono:h" .. fontSize
+    end)
+end
