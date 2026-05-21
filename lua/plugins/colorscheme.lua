30c3eb9fb3a613f6f24fd3c768428085c5c99b0f
Rudy Hartwig <rudyhartwig25@outlook.com>
2026-05-21 14:48:16 -0400

    update colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,22 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
+  "Shatur/neovim-ayu",
+  name = "ayu",
   priority = 1000,
   lazy = false,
   config = function()
-    vim.cmd.colorscheme("catppuccin-mocha")
+    require("ayu").setup({
+      overrides = {
+        Normal = { bg = "None" },
+        NormalFloat = { bg = "none" },
+        ColorColumn = { bg = "None" },
+        SignColumn = { bg = "None" },
+        Folded = { bg = "None" },
+        FoldColumn = { bg = "None" },
+        CursorLine = { bg = "None" },
+        CursorColumn = { bg = "None" },
+        VertSplit = { bg = "None" },
+      },
+    })
+    vim.cmd.colorscheme("ayu-dark")
   end,
 }
