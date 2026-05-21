6f26872c8eef965cabe071aa0d9aa7249f8afa7d
Rudy Hartwig <rudyhartwig25@outlook.com>
2026-05-21 16:16:40 -0400

    update ayu

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,17 +5,22 @@ return {
   lazy = false,
   config = function()
     require("ayu").setup({
-      overrides = {
-        Normal = { bg = "None" },
-        NormalFloat = { bg = "none" },
-        ColorColumn = { bg = "None" },
-        SignColumn = { bg = "None" },
-        Folded = { bg = "None" },
-        FoldColumn = { bg = "None" },
-        CursorLine = { bg = "None" },
-        CursorColumn = { bg = "None" },
-        VertSplit = { bg = "None" },
-      },
+      overrides = function()
+        local colors = require('ayu.colors')
+        colors.generate()
+        return {
+          ['@variable.builtin'] = { fg = colors.error },
+          Normal = { bg = "None" },
+          NormalFloat = { bg = "none" },
+          ColorColumn = { bg = "None" },
+          SignColumn = { bg = "None" },
+          Folded = { bg = "None" },
+          FoldColumn = { bg = "None" },
+          CursorLine = { bg = "None" },
+          CursorColumn = { bg = "None" },
+          VertSplit = { bg = "None" },
+        }
+      end,
     })
     vim.cmd.colorscheme("ayu-dark")
   end,
