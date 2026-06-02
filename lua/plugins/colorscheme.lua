d6efa60c6a2b80d24af9c9805f02768e11337414
Rudy Hartwig <rudyhartwig25@outlook.com>
2026-06-01 20:21:43 -0400

    update colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,27 +1,10 @@
 return {
-  "Shatur/neovim-ayu",
-  name = "ayu",
-  priority = 1000,
+  "nickkadutskyi/jb.nvim",
   lazy = false,
+  priority = 1000,
+  opts = {},
   config = function()
-    require("ayu").setup({
-      overrides = function()
-        local colors = require('ayu.colors')
-        colors.generate()
-        return {
-          ['@variable.builtin'] = { fg = colors.error },
-          Normal = { bg = "None" },
-          NormalFloat = { bg = "none" },
-          ColorColumn = { bg = "None" },
-          SignColumn = { bg = "None" },
-          Folded = { bg = "None" },
-          FoldColumn = { bg = "None" },
-          CursorLine = { bg = "None" },
-          CursorColumn = { bg = "None" },
-          VertSplit = { bg = "None" },
-        }
-      end,
-    })
-    vim.cmd.colorscheme("ayu-dark")
+    -- require("jb").setup({transparent = true})
+    vim.cmd("colorscheme jb")
   end,
 }
